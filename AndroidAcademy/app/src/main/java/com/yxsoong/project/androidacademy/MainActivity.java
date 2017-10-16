ca241a6485aab05b70eebd7e919e0fd9f8d144f3
Clarisse <limclarisse96@gmail.com>
2017-10-16 21:49:51 +0800

    main

--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/MainActivity.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yxsoong.project.androidacademy;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +11,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager =	getFragmentManager();
+        FragmentTransaction fragmentTransaction;
+
+        fragmentTransaction =	fragmentManager.beginTransaction();
+        MainViewFragment mainFragment	=	new MainViewFragment();
+        fragmentTransaction.add(R.id.mainLayout1,	mainFragment);
+        fragmentTransaction.commit();
     }
 }
