fe5f30ae80facc580ecd7a68058695ea0eafb189
Soong Yi Xuan <yxsoong@gmail.com>
2017-10-21 01:12:08 +0800

    a lot of edits

--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/Lesson1Activity.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/Lesson1Activity.java
@@ -7,12 +7,9 @@ import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class Lesson1Activity extends Activity implements XMLFragment.OnFragmentInteractionListener, PaletteFragment.OnFragmentInteractionListener{
-    View paletteFragment;
-    View.OnDragListener dragListenerPalette;
-    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +22,6 @@ public class Lesson1Activity extends Activity implements XMLFragment.OnFragmentI
         Fragment xmlFragment = new XMLFragment();
         fragmentTransaction.add(R.id.xmlFrameLayout, xmlFragment);
         fragmentTransaction.commit();
-
-        textView = findViewById(R.id.headerXML);
-
-        //textView.setOnDragListener(dragListener);
-
 
     }
 
