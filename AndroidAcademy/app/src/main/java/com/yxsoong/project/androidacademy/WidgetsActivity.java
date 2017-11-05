079a028143b6b9337c23c7e0b28c9a9a8b84f7da
Clarisse <limclarisse96@gmail.com>
2017-11-05 19:05:01 +0800

    widget pictures added, and tabs created

--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/WidgetsActivity.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/WidgetsActivity.java
@@ -2,15 +2,29 @@ package com.yxsoong.project.androidacademy;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 
 public class WidgetsActivity extends Activity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_widgets);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -19,5 +33,39 @@ public class WidgetsActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item){
         finish();
         return true;
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    WidgetTab1 tab1 = new WidgetTab1();
+                    return tab1;
+                case 1:
+                    WidgetTab2 tab2 = new WidgetTab2();
+                    return tab2;
+                case 2:
+                    WidgetTab3 tab3 = new WidgetTab3();
+                    return tab3;
+
+                default:
+                    return null;
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+
     }
 }
