8a82ee55b6143691c36e133e321e3e8c09d7cb71
Clarisse <limclarisse96@gmail.com>
2017-11-06 01:57:48 +0800

    a lot of edits

--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/LayoutIntroduction.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/LayoutIntroduction.java
@@ -26,25 +26,26 @@ public class LayoutIntroduction extends Activity {
 
         SpannableString ss1 = new SpannableString(words);
         ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
-        TextView textView = findViewById(R.id.layout1);
+        TextView textView = findViewById(R.id.layoutTxt1);
         textView.setText(ss1);
 
         words = "Declare UI elements in XML. Android provides a straightforward XML vocabulary that corresponds to the View classes and subclasses, such as those for widgets and layouts.";
 
         ss1 = new SpannableString(words);
         ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
-        TextView textView2 = findViewById(R.id.layout2);
+        TextView textView2 = findViewById(R.id.layoutTxt2);
         textView2.setText(ss1);
 
         words = "Instantiate layout elements at runtime. Your application can create View and ViewGroup objects (and manipulate their properties) programmatically.";
 
         ss1 = new SpannableString(words);
         ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
-        TextView textView3 = findViewById(R.id.layout3);
+        TextView textView3 = findViewById(R.id.layoutTxt3);
         textView2.setText(ss1);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         finish();
         return true;
+    }
 }
