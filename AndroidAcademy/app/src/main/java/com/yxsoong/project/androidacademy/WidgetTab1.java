a30ae9286e3a3972a87ca912ac5c419af82c4dcc
Clarisse <limclarisse96@gmail.com>
2017-11-05 19:13:39 +0800

    a lot of edits

--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/WidgetTab1.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/WidgetTab1.java
@@ -1,11 +1,17 @@
 package com.yxsoong.project.androidacademy;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static com.yxsoong.project.androidacademy.R.id.widget1;
 
 
 /**
@@ -23,7 +29,15 @@ public class WidgetTab1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_widget_tab1, container, false);
+        View view =  inflater.inflate(R.layout.fragment_widget_tab1, container, false);
+
+        String words = "What are Widgets?\n"+
+                "The Widget package contains mostly visual User-Interface elements that you can use on our Application Screen. In Android Studios, there are pre-included widgets that you are able to use in the .xml files, such as TextView, Buttons, and many more.";
+        SpannableString ss1 = new SpannableString(words);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE),0,25,0);
+        TextView introManifest2 = view.findViewById(widget1);
+        introManifest2.setText(ss1);
+        return view;
     }
 
 }
