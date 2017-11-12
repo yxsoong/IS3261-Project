f68fab0a7f46629069dba5a4dc29ae9a0f318754
Soong Yi Xuan <yxsoong@gmail.com>
2017-11-12 21:47:09 +0800

    gettingstarted

--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/GettingStartedActivity.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/GettingStartedActivity.java
@@ -1,10 +1,8 @@
 package com.yxsoong.project.androidacademy;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 
 public class GettingStartedActivity extends Activity {
@@ -14,14 +12,14 @@ public class GettingStartedActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getting_started);
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        finish();
-        return true;
-    }
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        finish();
+//        return true;
+//    }
 
     public void onClick_GoToCreateProjectActivity(View view){
         Intent myIntent = new Intent(this, CreateProjectActivity.class);
--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/MainRecyclerAdapter.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/MainRecyclerAdapter.java
@@ -1,5 +1,7 @@
 package com.yxsoong.project.androidacademy;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 
 public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapter.ViewHolder> {
 
+    private Context context;
     public MainRecyclerAdapter(){
 
     }
@@ -26,6 +29,7 @@ public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapte
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_menu_card, parent, false);
         ViewHolder viewHolder = new ViewHolder(v);
+        context = parent.getContext();
         return viewHolder;
     }
 
@@ -54,9 +58,16 @@ public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapte
             arrow = itemView.findViewById(R.id.itemArrow);
 
             itemView.setOnClickListener(new View.OnClickListener() {
+                Intent myIntent;
                 @Override
                 public void onClick(View v) {
                     int position = getAdapterPosition();
+                    switch (position){
+                        case 0:
+                            myIntent = new Intent(context, GettingStartedActivity.class);
+                            break;
+                    }
+                    context.startActivity(myIntent);
                 }
             });
         }
