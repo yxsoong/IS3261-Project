9bf37954d497de5456341930c79ca5f397589904
Clarisse <limclarisse96@gmail.com>
2017-11-13 03:15:36 +0800

    a lot of edits

--- AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/LayoutIntroduction.java
+++ AndroidAcademy/app/src/main/java/com/yxsoong/project/androidacademy/LayoutIntroduction.java
@@ -1,8 +1,8 @@
 package com.yxsoong.project.androidacademy;
 
-import android.support.v7.app.ActionBar;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -21,59 +21,59 @@ public class LayoutIntroduction extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         String words = "What is a layout?\n" +
-                "A layout defines the visual structure for a user interface, such as the UI for an activity or app widget. You can declare a layout in two ways:";
+                "A layout defines the visual structure for a user interface, such as the UI for an activity or app widget.\n You can declare a layout in two ways:";
 
         SpannableString ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 19, 0);
         TextView textView = findViewById(R.id.layoutTxt1);
         textView.setText(ss1);
 
-        words = "Declare UI elements in XML. Android provides a straightforward XML vocabulary that corresponds to the View classes and subclasses, such as those for widgets and layouts.";
+        words = "1. Declare UI elements in XML.";
 
         ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 30, 0);
         TextView textView2 = findViewById(R.id.layoutTxt2);
         textView2.setText(ss1);
 
-        words = "Instantiate layout elements at runtime. Your application can create View and ViewGroup objects (and manipulate their properties) programmatically.";
+        words = "2. Instantiate layout elements at runtime.";
 
         ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 42, 0);
         TextView textView3 = findViewById(R.id.layoutTxt3);
         textView3.setText(ss1);
 
-        words = "organizes its children into a single horizontal or vertical row. It creates a scrollbar if the length of the window exceeds the length of the screen.";
+        words = "Linear Layout\norganizes its children into a single horizontal or vertical row. It creates a scrollbar if the length of the window exceeds the length of the screen.";
 
         ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 13, 0);
         TextView textView4 = findViewById(R.id.layoutTxt4);
         textView4.setText(ss1);
 
-        words = "Enables you to specify the location of child objects relative to each other (child A to the left of child B) or to the parent (aligned to the top of the parent).";
+        words = "Relative Layout\nEnables you to specify the location of child objects relative to each other (child A to the left of child B) or to the parent (aligned to the top of the parent).";
 
         ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 15, 0);
         TextView textView5 = findViewById(R.id.layoutTxt5);
         textView5.setText(ss1);
 
-        words = "Displays web pages.";
+//        words = "Web Pages\nDisplays web pages.";
+//
+//        ss1 = new SpannableString(words);
+//        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 9, 0);
+//        TextView textView6 = findViewById(R.id.layoutTxt6);
+//        textView6.setText(ss1);
+
+        words = "Grid view\nDisplays a scrolling grid of columns and rows.";
 
         ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 5, 0);
-        TextView textView6 = findViewById(R.id.layoutTxt6);
-        textView6.setText(ss1);
-
-        words = "Displays a scrolling grid of columns and rows.";
-
-        ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 9, 0);
         TextView textView7 = findViewById(R.id.layoutTxt7);
         textView7.setText(ss1);
 
-        words = "Displays a scrolling single column list.";
+        words = "List View\nDisplays a scrolling single column list.";
 
         ss1 = new SpannableString(words);
-        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 20, 0);
+        ss1.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 9, 0);
         TextView textView8 = findViewById(R.id.layoutTxt8);
         textView8.setText(ss1);
 
